f5e23b6d8c21bcec183cb8df48c85a5eb63285c6
yerim shin <yrs1107@naver.com>
2022-04-04 22:22:14 +0900

    little changes in variables name

--- solutions/exercise1.h
+++ solutions/exercise1.h
@@ -13,9 +13,9 @@
 inline Eigen::Vector3d getEndEffectorPosition (const Eigen::VectorXd& gc) {
 
   //world_to_root
-  Eigen::Vector3d r_world_to_root(gc(0), gc(1), gc(2));
-  Eigen::Matrix3d R_world_to_root;
-  R_world_to_root = Eigen::Quaternion<double>(gc(3), gc(4), gc(5), gc(6)).toRotationMatrix();
+  Eigen::Vector3d r_world_root(gc(0), gc(1), gc(2));
+  Eigen::Matrix3d R_world_root;
+  R_world_root = Eigen::Quaternion<double>(gc(3), gc(4), gc(5), gc(6)).toRotationMatrix();
 
   //root_to_hip
   Eigen::Vector3d r_root_hip(-0.183f, 0.047f, 0.0f);
@@ -27,21 +27,23 @@ inline Eigen::Vector3d getEndEffectorPosition (const Eigen::VectorXd& gc) {
   Eigen::Matrix3d R_hip_thigh(Eigen::AngleAxisd(gc(17), Eigen::Vector3d::UnitY()));
 
   //thigh_to_calf
-  Eigen::Vector3d r_thigh_to_calf(0.0f, 0.0f, -0.2f);
-  Eigen::Matrix3d R_thigh_to_calf(Eigen::AngleAxisd(gc(18), Eigen::Vector3d::UnitY()));
+  Eigen::Vector3d r_thigh_calf(0.0f, 0.0f, -0.2f);
+  Eigen::Matrix3d R_thigh_calf(Eigen::AngleAxisd(gc(18), Eigen::Vector3d::UnitY()));
 
   //calf_to_ee
-  Eigen::Vector3d r_calf_to_ee(0.0f, 0.0f, -0.2f);
+  Eigen::Vector3d r_calf_ee(0.0f, 0.0f, -0.2f);
 
-  Eigen::Vector3d r_world_to_ee;
+  Eigen::Vector3d r_world_ee;
   
-  Eigen::Vector3d r_thigh_to_ee = r_thigh_to_calf + R_thigh_to_calf * (r_calf_to_ee);
-  Eigen::Vector3d r_hip_to_ee = r_hip_thigh + R_hip_thigh * (r_thigh_to_ee);
-  Eigen::Vector3d r_root_to_ee = r_root_hip + R_root_hip * r_hip_to_ee;
-  r_world_to_ee = r_world_to_root + R_world_to_root * r_root_to_ee;
+  Eigen::Vector3d r_thigh_ee = r_thigh_calf + R_thigh_calf * (r_calf_ee);
+  Eigen::Vector3d r_hip_ee = r_hip_thigh + R_hip_thigh * (r_thigh_ee);
+  Eigen::Vector3d r_root_ee = r_root_hip + R_root_hip * r_hip_ee;
+  r_world_ee = r_world_root + R_world_root * r_root_ee;
   
+Eigen::Vector3d r_world_hip = r_world_root + R_world_root * r_root_hip;
 
-  return r_world_to_ee;
+
+  return r_world_hip;
 }
 
 #endif //ME553_2022_SOLUTIONS_EXERCISE1_H_
